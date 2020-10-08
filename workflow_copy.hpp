d4f7a92a049363d1241e2294007d1d9e2aa86969
LinkClinton <LinkClinton@outlook.com>
2020-10-08 14:23:12 +0800

    change sln dir

--- workflow_copy.hpp
+++ workflow_copy.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "../references/directx-wrapper/directx12-wrapper/resources/texture2d.hpp"
-#include "../references/directx-wrapper/directx12-wrapper/resources/buffer.hpp"
+#include <directx-wrapper/directx12-wrapper/resources/texture2d.hpp>
+#include <directx-wrapper/directx12-wrapper/resources/buffer.hpp>
 
 #include "cores/workflow_independent.hpp"
 
